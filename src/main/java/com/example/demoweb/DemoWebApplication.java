76daeb9164b96a7cd3ec8f42c740122e32690a33
Artur <fartur09@mail.ru>
2023-02-12 09:48:21 +0500

    3.1

--- src/main/java/com/example/demoweb/DemoWebApplication.java
+++ src/main/java/com/example/demoweb/DemoWebApplication.java
@@ -2,6 +2,7 @@ package com.example.demoweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class DemoWebApplication {
